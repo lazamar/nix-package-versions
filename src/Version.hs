2e5edba2bfe82c237078a39f40ecd3043b63b15b
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-02-09 13:36:48 +0000

    Collect versions

--- src/Version.hs
+++ src/Version.hs
@@ -1,7 +1,18 @@
 module Version where
 
+import Data.Monoid ((<>))
 import System.Process (shell, readCreateProcess, CreateProcess(..))
 
 searchVersions :: FilePath -> IO String
 searchVersions path =
-    readCreateProcess ((shell "ls") { cwd = Just "/etc/" }) ""
+    let
+        command :: String
+        command =
+            "git rev-list master -- "
+            <> path
+            <> " | parallel -q git grep -E '^\\s+version\\s?=\\s?\"[^\"]+\"\\s*;\\s*$' -- "
+            <> path
+
+        nixpkgsRepoPath = "/Users/marcelo/Projects/nixpkgs"
+    in
+    readCreateProcess ((shell command) { cwd = Just nixpkgsRepoPath }) ""
