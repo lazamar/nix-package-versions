ad7023e935af63a66182a568ba05feeac29fd3ec
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-02-09 14:36:46 +0000

    Print with hash in lines

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,4 +9,5 @@ someFunc = do
     putStrLn "Type a package to find its versions: "
     package <- getLine
     version <- searchVersions package
-    print version
+    sequence $ fmap putStrLn version
+    return ()
--- src/Version.hs
+++ src/Version.hs
@@ -3,16 +3,16 @@ module Version where
 import Data.Monoid ((<>))
 import System.Process (shell, readCreateProcess, CreateProcess(..))
 
-searchVersions :: FilePath -> IO String
-searchVersions path =
+searchVersions :: FilePath -> IO [String]
+searchVersions path = do
     let
         command :: String
         command =
             "git rev-list master -- "
             <> path
-            <> " | parallel -q git grep -E '^\\s+version\\s?=\\s?\"[^\"]+\"\\s*;\\s*$' -- "
+            <> " | parallel -q git grep -E '^\\s+version\\s?=\\s?\"[^\"]+\"\\s*;\\s*$' {} -- "
             <> path
 
         nixpkgsRepoPath = "/Users/marcelo/Projects/nixpkgs"
-    in
-    readCreateProcess ((shell command) { cwd = Just nixpkgsRepoPath }) ""
+    out <- readCreateProcess ((shell command) { cwd = Just nixpkgsRepoPath }) ""
+    return $ lines out
