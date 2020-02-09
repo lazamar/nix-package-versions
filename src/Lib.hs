e5c912d9e52f3749fa3fdba56265f0d7a7f1c37e
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-02-09 19:49:55 +0000

    Finding package path in JSON package database

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,14 +2,21 @@ module Lib
     ( someFunc
     ) where
 
+import Data.HashMap.Strict as H
 import Version (searchVersions)
 import qualified PackageDB
 
 someFunc :: IO ()
 someFunc = do
     db <- PackageDB.generate "./unstable.json"
-    putStrLn "Type a package to find its versions: "
-    package <- getLine
-    version <- searchVersions package
-    sequence $ fmap (putStrLn . show) version
+
+    putStrLn "Type a package name to find its nixpkgs path: "
+    pname <- getLine
+    let packagePath = PackageDB.getInfo pname db
+    print packagePath
+
+    --putStrLn "Type a package to find its versions: "
+    --package <- getLine
+    --version <- searchVersions package
+    --sequence $ fmap (putStrLn . show) version
     return ()
--- src/PackageDB.hs
+++ src/PackageDB.hs
@@ -1,24 +1,51 @@
-module PackageDB (generate) where
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DerivingStrategies #-}
+
+module PackageDB (generate, getInfo) where
 
 {-|
     This module takes care of finding the path of a package in the
     nixpkgs repo
 -}
 
+import Data.Aeson (FromJSON(..), ToJSON, eitherDecodeFileStrict, withObject, (.:), (.:?))
+import Data.Hashable (Hashable)
 import Data.HashMap.Strict (HashMap)
+import GHC.Generics (Generic)
+
+import qualified Data.HashMap.Strict as H
 
 generate :: FilePath -> IO PackageDB
-generate filepath = do
+generate filePath = do
     putStrLn "Generating package database ..."
-    db <- return $ PackageDB mempty
+    db <- eitherDecodeFileStrict filePath
     putStrLn "Package database generated."
-    return db
+    return $ either error id db
 
-newtype PackageDB = PackageDB (HashMap PackageName PackageContent)
+getInfo :: String -> PackageDB -> Maybe PackageInfo
+getInfo str = H.lookup str . unPackageDB
+
+newtype PackageDB = PackageDB { unPackageDB :: HashMap PackageName PackageInfo }
+    deriving (Generic)
+
+instance ToJSON PackageDB
+
+instance FromJSON PackageDB where
+    parseJSON = withObject "PackageDB" $ \v ->
+        PackageDB <$> (v .: "packages")
 
 type PackageName = String
 
-data PackageContent = PackageContent
-    { name :: PackageName
-    , nixpkgsPath :: FilePath
-    }
+data PackageInfo = PackageInfo
+    { packageNixpkgsPath :: Maybe FilePath
+    } deriving (Show, Eq, Generic)
+
+instance Hashable PackageInfo
+
+instance ToJSON PackageInfo
+
+instance FromJSON PackageInfo where
+    parseJSON = withObject "PackageInfo" $ \v ->
+        PackageInfo <$> (v .: "meta" >>= (.:? "position"))
