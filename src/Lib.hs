5a7f80f18be5585480534e132386ef5709d2136f
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-02-09 20:06:43 +0000

    Iterate fetching multiple package paths

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Monad (unless)
 import Data.HashMap.Strict as H
 import Version (searchVersions)
 import qualified PackageDB
@@ -10,10 +11,15 @@ someFunc :: IO ()
 someFunc = do
     db <- PackageDB.generate "./unstable.json"
 
-    putStrLn "Type a package name to find its nixpkgs path: "
-    pname <- getLine
-    let packagePath = PackageDB.getInfo pname db
-    print packagePath
+    let getPkg = do
+            putStrLn "Type a package name to find its nixpkgs path: "
+            pname <- getLine
+            print $ PackageDB.getInfo pname db
+            putStrLn "Continue? (y/n)"
+            v <- getLine
+            unless ("n" == v) getPkg
+
+    getPkg
 
     --putStrLn "Type a package to find its versions: "
     --package <- getLine
