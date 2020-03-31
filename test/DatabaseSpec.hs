c563c1e37dd40ffbe8b4c3271d55b09376ee1984
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-03-31 22:33:21 +0100

    Update tests

--- test/DatabaseSpec.hs
+++ test/DatabaseSpec.hs
@@ -1,38 +1,49 @@
 module DatabaseSpec (spec) where
 
+import Control.Exception (bracket)
 import Control.Exception (evaluate)
+import Data.Text (pack)
+import Data.Time.Calendar (Day(ModifiedJulianDay))
 import Nix.Revision (Revision(..), Package(..))
-import Nix.Versions.Types (Hash(..), Version(..), Name(..), Commit(..))
+import Nix.Versions.Types (DBFile(..), CachePath(..), Hash(..), Version(..), Name(..), Commit(..))
+import System.IO.Temp (withSystemTempDirectory)
 import System.IO.Temp (withSystemTempFile)
+import System.Posix.Files (removeLink)
 import Test.Hspec (Spec, describe, it, shouldBe)
 import qualified Data.HashMap.Strict as HM
 import qualified Nix.Versions.Database as P
-import Data.Text (pack)
-import Data.Time.Calendar (Day(ModifiedJulianDay))
-import Control.Exception (bracket)
 
 
 -- | Create a temporary database file and connect to it to perform tests.
 -- Erases the file after tests are finished
 overDatabase :: (P.Connection -> IO a) -> IO a
 overDatabase f =
-    withSystemTempFile "NIX_TEST_DB" $ \filePath _ ->
-        bracket (P.connect $ pack filePath) P.disconnect f
+    withSystemTempDirectory "NIX_TEST" $ \dirPath ->
+        bracket (P.connect (CachePath dirPath) (DBFile "Test.db")) P.disconnect f
+
+day :: Day
+day = read "2020-03-31"
 
 spec :: Spec
 spec = do
     describe "Database" $ do
+        let pname = Name "my-package"
+            pkg   = Package Nothing (Version "1.0") Nothing
+            commit = Commit (Hash "hash") (ModifiedJulianDay 10)
+            revision = Revision commit $ HM.fromList [(pname, pkg)]
+
+        it "can save and load a revision" $ do
+            overDatabase $ \conn -> do
+                () <- P.save conn day revision
+                v1 <- P.versions conn pname
+                length v1 `shouldBe` 1
+
         -- We can add the same thing over and over again and we won't get duplicates
         it "Adding revisions is idempotent" $ do
             overDatabase $ \conn -> do
-                let pname = Name "my-package"
-                    pkg   = Package Nothing (Version "1.0") Nothing
-                    commit = Commit (Hash "hash") (ModifiedJulianDay 10)
-                    revision = Revision commit $ HM.fromList [(pname, pkg)]
-
-                () <- P.save conn revision
+                () <- P.save conn day revision
                 v1 <- P.versions conn pname
-                () <- P.save conn revision
+                () <- P.save conn day revision
                 v2 <- P.versions conn pname
                 v1 `shouldBe` v2
                 length v1 `shouldBe` 1
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,6 @@
 module Main where
 
 import Data.Time.Calendar (Day)
-import Nix.Revision (commitToFileName, fileNameToCommit)
-import Nix.Versions.Types (Hash(..), Version(..), Name(..), Commit(..))
 import Test.Hspec (hspec, Spec, describe, it, shouldBe)
 
 import qualified DatabaseSpec
@@ -10,26 +8,4 @@ import qualified DatabaseSpec
 main :: IO ()
 main = hspec $ do
     DatabaseSpec.spec
-    commitFileName
-
---packageVersions :: Spec ()
---packageVersions =
-    --describe "Package version parsing handles" $ do
-        --it "packages with only one number" $ do
-            --let pkg = "1f779011e9986a290dc252dce85d9a162968d5d5:pkgs/development/compilers/ghc/8.2.1.nix:  version = \"1\";"
-            --return ()
-        --it "packages with numbers and dots" $ do
-            --let pkg = "2b1eabc14507be020d52c2ecb08da7a0f217a82f:pkgs/development/compilers/purescript/purescript/default.nix:  version = \"0.13.0\";"
-            --return ()
-        --it "packages with letters before numbers" $ do
-            --let pkg = "46a181a602e6e036736dbf3171fbd0c2e2df982b:pkgs/development/compilers/purescript/purescript/default.nix:  version = \"v0.12.4\";"
-        --it "packages with letters, numbers and dashes"
-            --let pkg = "1f779011e9986a290dc252dce85d9a162968d5d5:pkgs/development/compilers/ghc/8.2.1.nix:  version = \"8.2.1-rc3\";"
-
-commitFileName :: Spec
-commitFileName =
-    describe "Commit file name" $ do
-        it "encodes and decodes to the same thing" $ do
-            let commit = Commit (Hash "9a2898c6d1406489c1d691ecbbb3585888c1c613") (read "2020-03-30")
-            fileNameToCommit (commitToFileName commit) `shouldBe` Just commit
 
