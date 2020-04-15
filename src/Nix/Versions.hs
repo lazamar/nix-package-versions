94cf90ce3acb856c7c1436df8e07417948c902e6
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-04-15 23:51:40 +0100

    Use monad transformers for Version

--- src/Nix/Versions.hs
+++ src/Nix/Versions.hs
@@ -7,9 +7,11 @@ module Nix.Versions
     ( savePackageVersionsForPeriod
     ) where
 
-import Control.Concurrent.Async (mapConcurrently)
 import Control.Monad.Except (liftIO, liftEither, runExceptT, runExcept)
 import Control.Monad (join)
+import Control.Monad.IO.Class (MonadIO(..))
+import Control.Concurrent.Classy (MonadConc)
+import Control.Concurrent.Classy.Async (mapConcurrently)
 import Data.Time.Calendar (Day, fromGregorian, toGregorian, showGregorian)
 import Data.Time.Calendar.WeekDate (toWeekDate, fromWeekDate)
 import Data.Either (partitionEithers)
@@ -29,10 +31,10 @@ import qualified Nix.Revision as Revision
 -- | Download lists of packages and their versions
 -- for commits between 'to' and 'from' dates and save them to
 -- the database.
-savePackageVersionsForPeriod :: Config -> Day -> Day -> IO [Either String Commit]
+savePackageVersionsForPeriod :: (MonadConc m, MonadIO m) => Config -> Day -> Day -> m [Either String Commit]
 savePackageVersionsForPeriod (Config dbFile cacheDir gitUser) from to = do
-    conn <- DB.connect cacheDir dbFile
-    revisions <- DB.revisions conn
+    conn      <- liftIO $ DB.connect cacheDir dbFile
+    revisions <- liftIO $ DB.revisions conn
     let
         weeksAlreadyInDB :: Set (Year, Week)
         weeksAlreadyInDB
@@ -53,9 +55,8 @@ savePackageVersionsForPeriod (Config dbFile cacheDir gitUser) from to = do
         daysNeeded = toDay <$> weeksNeeded
 
     res <- mapConcurrently (f conn) daysNeeded
-    DB.disconnect conn
+    liftIO $ DB.disconnect conn
     return res
-
     where
         wasAttempted (_,_,state) = case state of
             Success         -> True
@@ -65,7 +66,7 @@ savePackageVersionsForPeriod (Config dbFile cacheDir gitUser) from to = do
         revDate (day, _, _) = day
 
         f conn day = do
-            eCommits <- Revision.commitsUntil gitUser day
+            eCommits <- liftIO $ Revision.commitsUntil gitUser day
             case eCommits of
                 Left err -> return $ Left $ "Unable to get commits from GitHub for " <> show day <> ": " <> show err
                 Right commits ->
@@ -78,18 +79,18 @@ savePackageVersionsForPeriod (Config dbFile cacheDir gitUser) from to = do
                         $ zip [1..] commits
 
         announce day (tryCount, commit@(Commit hash _)) = do
-            putStrLn $ "Attempt " <> show tryCount <> ". Downloading files for " <> showGregorian day <> ". " <> show hash
+            liftIO $ putStrLn $ "Attempt " <> show tryCount <> ". Downloading files for " <> showGregorian day <> ". " <> show hash
             return commit
 
         download conn day commit = do
-            eRev <- Revision.build commit
+            eRev <- liftIO $ Revision.build commit
             case eRev of
                 Left  err -> do
-                    DB.registerInvalidRevision conn day commit
+                    liftIO $ DB.registerInvalidRevision conn day commit
                     return (Left (day, err))
                 Right rev -> do
-                    putStrLn $ "Saving Nix result for" <> show commit
-                    DB.save conn day rev
+                    liftIO $ putStrLn $ "Saving Nix result for" <> show commit
+                    liftIO $ DB.save conn day rev
                     return $ Right $ Revision.commit rev
 
 newtype Week = Week Int
@@ -109,11 +110,11 @@ toWeek day = (Year year, Week week)
 toDay :: (Year, Week) -> Day
 toDay (Year year, Week week) = fromWeekDate year week 1
 
-tryInSequence :: Show e => e -> [IO (Either e a)] -> IO (Either String a)
+tryInSequence :: (Show e, MonadIO m) => e -> [m (Either e a)] -> m (Either String a)
 tryInSequence err l = go 0 l
     where
         go n [] = return $ Left $ "Failed after " <> show n <> " attempts with: " <> show err
-        go n (x:xs) = x >>= either (\e -> print e >> go (n + 1) xs) (return . Right)
+        go n (x:xs) = x >>= either (\e -> liftIO (print e) >> go (n + 1) xs) (return . Right)
 
 
 -- | We are not interested in every week of the year.
