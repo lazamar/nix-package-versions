3747c0fc1de865d107375217254339321953c1a0
Marcelo Lazaroni <lazamar@fb.com>
2023-03-11 21:51:12 +0000

    Add new interface methods to Storage

--- src/App/Storage.hs
+++ src/App/Storage.hs
@@ -3,6 +3,7 @@ module App.Storage where
 
 import Data.Aeson (FromJSON, ToJSON)
 import Data.Time.Calendar (Day(..))
+import Data.Time.Clock.POSIX (POSIXTime)
 import GHC.Generics (Generic)
 
 import Data.Git (Hash, Commit)
@@ -19,6 +20,13 @@ data CommitState
 instance ToJSON CommitState
 instance FromJSON CommitState
 
+data Coverage = Coverage Period Revision CommitState
+
+data Period = Period
+  { start :: POSIXTime
+  , end :: POSIXTime
+  }
+
 class Storage s where
   -- read
   versions :: s -> Channel -> Package -> IO [(PackageDetails, Hash, Day)]
@@ -30,7 +38,13 @@ class Storage s where
 
   writeCommitPackages :: s -> Commit -> [PackageDetails] -> IO ()
   writeCommitState :: s -> Commit -> CommitState -> IO ()
-  writeCoverage :: s -> Day -> Channel -> Commit -> IO ()
+
+
+  coverage :: s -> Channel -> IO [Coverage]
+  versions' :: s -> Channel -> Package -> IO [(PackageDetails, Commit)]
+
+  writeCoverage :: s -> Coverage -> IO ()
+  writePackage :: s -> Commit -> PackageDetails -> IO ()
 
 data Database = forall s. Storage s => Database s
 
@@ -41,4 +55,5 @@ instance Storage Database where
       writePackages s day revision packages
     writeRevisionState (Database s) day revision state =
       writeRevisionState s day revision state
+
 
